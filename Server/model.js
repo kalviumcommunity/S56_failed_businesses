1814afe1fd0283b1ca69e4552d2c7e20c1ee17d9
bhagirathauti <bhagirath961778@gmail.com>
2024-03-02 17:14:07 +0530

    data fetching

--- Server/model.js
+++ Server/model.js
@@ -1,1 +1,10 @@
 const mongoose = require("mongoose")
+
+const businessSchema = new mongoose.Schema({
+    id:Number,
+    name:String,
+    owner:String
+})
+
+const businesses = mongoose.model("dataone",businessSchema)
+module.exports = businesses
